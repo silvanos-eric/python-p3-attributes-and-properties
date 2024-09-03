1cf9eadf2686cd3cc21de8909c3e1c45252c207b
Silvanos Eric <ericoderito@gmail.com>
2024-09-03 14:58:43 +0300

    feat(lib): add property methods to Dog and Person classes
    Added __init__ constructors and property methods for `name` and `breed`
    in the Dog class, and `name` and `job` in the Person class. Validation
    has been implemented to ensure that name length is between 1 and 25
    characters and that breed/job is within the list of approved values.

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = 'John Doe', job = 'Admin'):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print('Name must be string between 1 and 25 characters.')
+        else:
+            self._name = name.title()
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+        if job not in approved_jobs:
+            print('Job must be in list of approved jobs.')
+        else:
+            self._job = job
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
